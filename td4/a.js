89078fb7d28712997ddde88f50ef8d2614feb078
Noah <144342794+NoahNob@users.noreply.github.com>
2023-10-12 16:44:06 +0200

    début td4

--- td4/a.js
+++ td4/a.js
@@ -1,9 +1,39 @@
 'use strict'
 
-function NUL(){
-    alert("Je n ai pas aimé les tortues ninja")
+const $=function(id){
+    return document.getElementById(id);
+};
+
+
+const bt = $("bt-hello");
+bt.addEventListener("click", function f(){
+    const randomColor = Math.floor(Math.random()*16777215).toString(16);
+    document.body.style.backgroundColor = "#" + randomColor;
+});
+$("bt-clear").addEventListener("click", function(){
+    document.body.innerHTML="";
+});
+
+const $=(id)=>document.getElementById(id);
+
+const addEvt=function (type,id,listener){
+    const elm=$(id);
+    elm && elm.addEventListener-(type,listener);
 }
-const bt= document.body.querySelector("button");
-bt.addEventListener("click", NUL(){
-    
+
+addEvt("click", "bt-bg", function(){
+    const color=$("selected-color").value;
+    document.body.style.backgroundColor=color;
 })
+
+
+
+$("bt-dom").addEventListener("click", function(){
+    const h2=document.createElement("h2");
+    h2.innerText=("SOUS TITRE AJOUTÉ DYNAMIQUEMENT");
+    $("content").appendChild(h2);
+    const img=document.createElement("img");
+    img.src="https://media.tenor.com/tFIicz8nikQAAAAC/auuuuuuhhhh-aaaaaaa.gif"
+    img.style.width="200px"
+    $("content").appendChild(img);
+});
